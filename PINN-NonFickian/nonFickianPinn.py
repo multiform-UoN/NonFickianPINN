49a801823e621cadb07a52967b960cacd3919ca2
Matteo Icardi <mat.icardi@gmail.com>
2023-11-22 12:54:54 +0000

    chatGPT optimisation

--- PINN-NonFickian/nonFickianPinn.py
+++ PINN-NonFickian/nonFickianPinn.py
@@ -13,18 +13,24 @@ import os
 
 #%%
 # USER INPUTS
-# Testcase (choose the one you want to run)
-testcase = "testcase1"
-pde_weight = 1.0
-data_weight = 1.0
-ic_weight = 100.0
-bc_weight = 10.0
-learning_rate = 6e-4
-epochs = 1000
-train_parameters = True
-num_hidden_layers = 20
+
+testcase = "testcase2" # Testcase (choose the one you want to run)
+
+pde_weight = 1.0      # penalty for the PDE
+data_weight = 1.0     # penalty for the data fitting
+ic_weight = 100.0     # penalty for the initial condition
+bc_weight = 100.0     # penalty for the boundary condition
+
+learning_rate = 1e-3  # learning rate for the network weights
+epochs = 3000         # number of epochs
+num_hidden_layers = 30
 num_neurons_per_layer = 100
-activation = 'sigmoid' # 'sigmoid' or 'tanh'
+activation = 'tanh' # 'sigmoid' or 'tanh'
+
+train_parameters = True # train the parameters or not
+def learning_rate_parameters(learning_rate, epoch): # learning rate for parameters
+    return learning_rate*0
+param_perturbation = 1e-2 # perturbation for the parameters
 
 # Check if the folder exists
 datafolder = "../data/"+testcase
@@ -32,37 +38,52 @@ if not os.path.exists(datafolder):
     # If it doesn't exist, use the folder name without the "../"
     datafolder = "data/"+testcase
 
-# Load data
+# Load data as pandas dataframes
 p = pd.read_csv(f'{datafolder}/p.csv', header=None)
 x_data = pd.read_csv(f'{datafolder}/x.csv', header=None)
 t_data = pd.read_csv(f'{datafolder}/t.csv', header=None)
 c_data = pd.read_csv(f'{datafolder}/c.csv', header=None)
 c1_data = pd.read_csv(f'{datafolder}/c1.csv', header=None)
 
+# mesh and time discretization
 nt = np.shape(t_data)[0]
 nx = np.shape(x_data)[0]
-
 X_data, T_data = np.meshgrid(x_data, t_data)
 Xgrid = np.vstack([X_data.flatten(), T_data.flatten()]).T
 Xgrid_data = Xgrid[:, 0]
 Tgrid_data = Xgrid[:, 1]
 
+# convert data to numpy arrays and float32
 p = np.array(p).squeeze().astype(np.float32)
 x_train = Xgrid_data.astype(np.float32)
 t_train = Tgrid_data.astype(np.float32)
 c_train = c_data.astype(np.float32)
 c1_train = c1_data.astype(np.float32)
 
+# Convert data to tensor because tf.GradientTape() can only watch tensor and not numpy arrays
+x_train = tf.expand_dims(tf.convert_to_tensor(x_train), -1)
+t_train = tf.expand_dims(tf.convert_to_tensor(t_train), -1)
+c_data_train = tf.convert_to_tensor(c_train)
+c1_data_train = tf.convert_to_tensor(c1_train)
+
+# tf differentiation variables
+tt = tf.Variable(t_train)
+xx = tf.Variable(x_train)
+
+# model inputs
+inputs = [xx, tt, c_data_train, c1_data_train]
+
+
 # additional variables added to gradient tracking
-beta0 = tf.Variable([p[0]], trainable=train_parameters)
-d = tf.Variable([p[1]], trainable=train_parameters)
-u = tf.Variable([p[2]], trainable=train_parameters)
-beta1 = tf.Variable([p[3]], trainable=train_parameters)
-lambda1 = tf.Variable([p[4]], trainable=train_parameters)
+randp =  (p * (param_perturbation * np.random.randn(p.size) + 1)).astype(np.float32)
+beta0 = tf.Variable([randp[0]], trainable=train_parameters)
+d = tf.Variable([randp[1]], trainable=train_parameters)
+u = tf.Variable([randp[2]], trainable=train_parameters)
+beta1 = tf.Variable([randp[3]], trainable=train_parameters)
+lambda1 = tf.Variable([randp[4]], trainable=train_parameters)
 
 #%%
 # Define the PINN model
-
 def pinn_model(num_hidden_layers=num_hidden_layers, num_neurons_per_layer=num_neurons_per_layer):
     x_input = keras.Input(shape=(1,))
     t_input = keras.Input(shape=(1,))
@@ -85,26 +106,32 @@ def pinn_model(num_hidden_layers=num_hidden_layers, num_neurons_per_layer=num_ne
 
     return keras.Model(inputs=[t_input, x_input], outputs=output_c)
 
-
-# @tf.function
+@tf.function
 def custom_loss(inputs, model):
-    x, t, c_data, c1_data = inputs
 
+    # inputs
+    xx, tt, c_data, c1_data = inputs
+    
     # Compute derivatives:
-    tt = tf.Variable(t)
-    xx = tf.Variable(x)
     with tf.GradientTape(persistent=True) as tape:
+        # # # variables
+        # tt = tf.Variable(t)
+        # xx = tf.Variable(x)
+        tape.watch(tt)
+        tape.watch(xx)
+        # output model
         output_model = model([tt, xx])
         c_model = output_model[:, 0]
         c_model = tf.expand_dims(c_model, -1)
         c1_model = output_model[:, 1]
         c1_model = tf.expand_dims(c1_model, -1)
-        tape.watch(tt)
-        tape.watch(xx)
+        # derivatives and fluxes
         c_x = tape.gradient(c_model, xx)
         c_t = tape.gradient(c_model, tt)
         c1_t = tape.gradient(c1_model, tt)
         div_output = u * c_x - d * tape.gradient(c_x, xx)
+
+    del tape
 
     # Compute the components of loss function
     pde_loss_c = tf.reduce_mean(
@@ -120,7 +147,6 @@ def custom_loss(inputs, model):
     ic_fitting_loss = ic_c_fitting_loss + ic_c1_fitting_loss
     loss = pde_weight*pde_loss + data_weight*data_fitting_loss + ic_weight*ic_fitting_loss + bc_weight*data_bc_fitting_loss
 
-    del tape
 
     return loss, pde_loss, data_fitting_loss, ic_c1_fitting_loss, data_bc_fitting_loss
 
@@ -144,7 +170,7 @@ if train_parameters:
 # learning_rate = tf.keras.optimizers.schedules.PiecewiseConstantDecay([100, 300], [1e-2, 1e-3, 5e-4])
 optimizer = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
 
-# Training loop
+# some lists to save the losses and parameters
 losses = []
 pde_losses = []
 data_fitting_losses = []
@@ -155,55 +181,53 @@ beta1_values = []
 lambda1_values = []
 u_values = []
 d_values = []
-# sigma_values = []
 
-
-# Convert data to tensor because tf.GradientTape() can only watch tensor and not numpy arrays
-x_train = tf.expand_dims(tf.convert_to_tensor(x_train), -1)
-t_train = tf.expand_dims(tf.convert_to_tensor(t_train), -1)
-c_data_train = tf.convert_to_tensor(c_train)
-c1_data_train = tf.convert_to_tensor(c1_train)
-inputs = [x_train, t_train, c_data_train, c1_data_train]
+# Start loop and timer
 stop = False
-
-# Start timer
 t0 = time()
+
+# Epoch loop
 for epoch in range(epochs):
     if not stop:
         print("# STARTING EPOCH", epoch + 1)
 
+        # Compute the gradients
         with tf.GradientTape() as tape:
+            # Call the tf decorated loss function
             loss, pde_loss, data_fitting_loss, ic_fitting_loss, data_bc_fitting_loss = custom_loss(inputs, model)
-
-        print("Computing gradients")
-        gradients = tape.gradient(loss, trainable)
-        print("Applying gradients")
+            gradients = tape.gradient(loss, trainable)
+        
+        # Apply the gradients to update the weights
         optimizer.apply_gradients(zip(gradients, trainable))
-        print("Appending losses")
-        losses.append(loss.numpy())
-        pde_losses.append(pde_loss.numpy())
-        data_fitting_losses.append(data_fitting_loss.numpy())
-        data_bc_fitting_losses.append(data_bc_fitting_loss.numpy())
-        ic_fitting_losses.append(ic_fitting_loss.numpy())
-        beta0_values.append(beta0.numpy())
-        beta1_values.append(beta1.numpy())
-        lambda1_values.append(lambda1.numpy())
-        u_values.append(u.numpy())
-        d_values.append(d.numpy())
+        
+        del tape
 
-        current_lr = optimizer._decayed_lr(tf.float32).numpy()
-        if train_parameters: # perhaps this needs to be applied only after a certain number of epochs
+        # Update parameters
+        current_lr = learning_rate_parameters(optimizer.learning_rate.numpy(), epoch)
+        # perhaps this needs to be applied only after a certain number of epochs
+        if train_parameters: 
             beta0.assign_sub(gradients[-5] * current_lr)
             beta1.assign_sub(gradients[-4] * current_lr)
             lambda1.assign_sub(gradients[-3] * current_lr)
             u.assign_sub(gradients[-2] * current_lr)
             d.assign_sub(gradients[-1] * current_lr)
 
+        # Save the losses parameters and outputs to screen
         if epoch % 1 == 0:
+            losses.append(loss.numpy())
+            pde_losses.append(pde_loss.numpy())
+            data_fitting_losses.append(data_fitting_loss.numpy())
+            data_bc_fitting_losses.append(data_bc_fitting_loss.numpy())
+            ic_fitting_losses.append(ic_fitting_loss.numpy())
+            beta0_values.append(beta0.numpy())
+            beta1_values.append(beta1.numpy())
+            lambda1_values.append(lambda1.numpy())
+            u_values.append(u.numpy())
+            d_values.append(d.numpy())
             print(f"Epoch {epoch + 1}/{epochs}, Loss: {loss.numpy()}")
-            print(
-                f"beta0={beta0.numpy()}, beta1={beta1.numpy()}, lambda1={lambda1.numpy()}, u={u.numpy()}, d={d.numpy()}")
+            print(f"beta0={beta0.numpy()}, beta1={beta1.numpy()}, lambda1={lambda1.numpy()}, u={u.numpy()}, d={d.numpy()}")
 
+        # Check if the loss is not decreasing anymore
         if len(losses) > 2 and (np.abs(losses[-1] - losses[-2]))/np.abs(losses[-2]) < 1e-8:
             stop = True
 
